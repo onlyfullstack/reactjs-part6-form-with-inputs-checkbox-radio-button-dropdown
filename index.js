11ce753dd8e9167fe6e1015907d40b71a866291a
onlyfullstack <onlyfullstackdev@gmail.com>
2019-11-12 10:08:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ class App extends Component {
     return (
       <div>
         <Form/>
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          ReactJS Tutorial on Only Fullstack
+        </a>
+        <a
+          href="https://onlyfullstack.blogspot.com/2019/11/react-tutorial.html"
+          target="_blank">
+          <img
+            src="https://1.bp.blogspot.com/-I276A9NSgjY/Xck5TadIjpI/AAAAAAAAAi0/gFb-9achXtk4XkELxVyRNR3UXuMJGsUfQCLcBGAsYHQ/s640/React%2BTutorial.jpg" height="30%" width="90%"/>
+        </a>
       </div>
     );
   }
